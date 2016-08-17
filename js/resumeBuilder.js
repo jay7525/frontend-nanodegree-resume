2c3562282ee61aa75cc6e964417f4ed7988373bc
jay c <jaymast@icloud.com>
2016-08-17 17:12:48 +0800

    Add bio code with object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,30 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// var awesomeThoughts = "I am JC awesome";
+
+// var funThoughts = awesomeThoughts.replace("awesome","fun");
+
+// console.log(funThoughts);
+
+// $("#main").append(funThoughts);
+
+
+// var name = "Jaymast"
+// var formattedName = HTMLheaderName.replace("%data%", name);
+
+// var role = " Web Developer";
+// var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
+
+var skill = {"HTML", "CSS", "Bootstrap", "Sketch"};
+
+var bio = {
+	"name" = "Jamie",
+	"role" = "Web developer",
+	"contact" = "jay@hotmail.com"
+	"picture" = "http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg",
+	"Welcome message" = "http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg",
+	"Skills" = skill
+};
+
+$("#header").append("bio");
