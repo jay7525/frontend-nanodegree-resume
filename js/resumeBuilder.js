f7559ac3ba345086ee004eba95d7bd8ad6ea0fc9
jay c <jaymast@icloud.com>
2016-08-20 12:12:36 +0800

    Clear old code, add bio and education

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,30 +1,35 @@
-// var awesomeThoughts = "I am JC awesome";
-
-// var funThoughts = awesomeThoughts.replace("awesome","fun");
-
-// console.log(funThoughts);
-
-// $("#main").append(funThoughts);
-
-
-// var name = "Jaymast"
-// var formattedName = HTMLheaderName.replace("%data%", name);
-
-// var role = " Web Developer";
-// var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-// $("#header").prepend(formattedRole);
-// $("#header").prepend(formattedName);
-
-var skill = {"HTML", "CSS", "Bootstrap", "Sketch"};
-
 var bio = {
-	"name" = "Jamie",
-	"role" = "Web developer",
-	"contact" = "jay@hotmail.com"
-	"picture" = "http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg",
-	"Welcome message" = "http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg",
-	"Skills" = skill
+	"name" : "Jamie",
+	"role" : "Web developer",
+	"contact" : {
+		"mobile" : "56547887",
+		"email" : "jay@dfskf.com",
+		"github" : "jay8367",
+		"location" : "Hong Kong"
+	},
+	"welcomeMessage" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+	"picture" : "http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg",
+	"Skills" : ["skill", "asdfs", "fddsf"]
 };
 
-$("#header").append("bio");
+
+var education ={
+"school":[
+	{
+		"name" : "WSCAD",
+		"location" : "London",
+		"degree": "BA",
+	    "majors": ["photography"],
+	    "dates": 2006,
+	    "url": "http:www.apple.com"
+	}
+],
+"onlineCourses" :[
+	{
+		"title" : "FEWD",
+		"school" : "Udacity",
+		"dates": 2016,
+	    "url": "http:www.udacity.com"
+	}
+]
+}
