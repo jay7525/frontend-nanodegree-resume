7f569e64146e4b6aca99460bb4aa27ca1a0c69ac
jay c <jaymast@icloud.com>
2016-08-22 11:39:40 +0800

    Add bio pic and  welcome message

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,33 @@
+// var awesomeThoughts = "I am JC awesome";
+
+// var funThoughts = awesomeThoughts.replace("awesome","fun");
+
+// console.log(funThoughts);
+
+// $("#main").append(funThoughts);
+
+
+var name = "Jaymast"
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = " Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+// var bio = {
+// 	"name" = "Jamie",
+// 	"role" = "Web developer",
+// 	"contact" = "jay@hotmail.com"
+// 	"picture" = "http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg",
+// 	"Welcome message" = "http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg",
+// 	"Skills" = skill
+// };
+
+// $("#header").append("bio");
+//...................
+
 var bio = {
 	"name" : "Jamie",
 	"role" : "Web developer",
@@ -7,54 +37,62 @@ var bio = {
 		"github" : "jay8367",
 		"location" : "Hong Kong"
 	},
-	"welcomeMessage" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+	"welcomeMessage" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
 	"picture" : "http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg",
 	"Skills" : ["skill", "asdfs", "fddsf"]
 };
 
 
-var education ={
-	"school":[
-	{
-		"name" : "WSCAD",
-		"location" : "London",
-		"degree": "BA",
-	    "majors": ["photography"],
-	    "dates": 2006,
-	    "url": "http:www.apple.com"
-	}
-	],
-	"onlineCourses" :[
-	{
-		"title" : "FEWD",
-		"school" : "Udacity",
-		"dates": 2016,
-	    "url": "http:www.udacity.com"
-	}
- 	]
-};
+$("#topContacts").append(HTMLmobile.replace("%data%", bio.contact.mobile));
+$("#topContacts").append(HTMLemail.replace("%data%", bio.contact.email));
+$("#topContacts").append(HTMLgithub.replace("%data%", bio.contact.github));
+$("#topContacts").append(HTMLlocation.replace("%data%", bio.contact.location));
 
-var work ={
-	"jobs":[
-	{
-		"employer" : "Freelance",
-		"title" : "Developer",
-		"location": "HK",
-	    "dates": "In Progress",
-	    "description": "All kinds of stuff"
-	}
-	]
-};
+$("#header").append(HTMLbioPic.replace("%data%", bio.picture));
+$("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
+
+// var education ={
+// 	"school":[
+// 	{
+// 		"name" : "WSCAD",
+// 		"location" : "London",
+// 		"degree": "BA",
+// 	    "majors": ["photography"],
+// 	    "dates": 2006,
+// 	    "url": "http:www.apple.com"
+// 	}
+// 	],
+// 	"onlineCourses" :[
+// 	{
+// 		"title" : "FEWD",
+// 		"school" : "Udacity",
+// 		"dates": 2016,
+// 	    "url": "http:www.udacity.com"
+// 	}
+//  	]
+// };
+
+// var work ={
+// 	"jobs":[
+// 	{
+// 		"employer" : "Freelance",
+// 		"title" : "Developer",
+// 		"location": "HK",
+// 	    "dates": "In Progress",
+// 	    "description": "All kinds of stuff"
+// 	}
+// 	]
+// };
 
 
 
-var projects = {
-"proj":[
-	{
-		"title" : "UX design",
-		"dates" : 2016,
-		"description": "UX for website",
-		"images": ["https://img1.wsimg.com/fos/sales/cwh/8/images/cats-with-hats-shop-02.jpg", "https://img1.wsimg.com/fos/sales/cwh/8/images/cats-with-hats-shop-02.jpg"]
-	}
-	]
-};
+// var projects = {
+// "proj":[
+// 	{
+// 		"title" : "UX design",
+// 		"dates" : 2016,
+// 		"description": "UX for website",
+// 		"images": ["https://img1.wsimg.com/fos/sales/cwh/8/images/cats-with-hats-shop-02.jpg", "https://img1.wsimg.com/fos/sales/cwh/8/images/cats-with-hats-shop-02.jpg"]
+// 	}
+// 	]
+// };
