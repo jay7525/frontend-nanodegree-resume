7a0caaee53506556d75430d6a853f93cb51eb788
jay c <jaymast@icloud.com>
2016-08-22 12:18:48 +0800

    Add skills

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,12 +1,3 @@
-// var awesomeThoughts = "I am JC awesome";
-
-// var funThoughts = awesomeThoughts.replace("awesome","fun");
-
-// console.log(funThoughts);
-
-// $("#main").append(funThoughts);
-
-
 var name = "Jaymast"
 var formattedName = HTMLheaderName.replace("%data%", name);
 
@@ -16,17 +7,6 @@ var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
-// var bio = {
-// 	"name" = "Jamie",
-// 	"role" = "Web developer",
-// 	"contact" = "jay@hotmail.com"
-// 	"picture" = "http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg",
-// 	"Welcome message" = "http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg",
-// 	"Skills" = skill
-// };
-
-// $("#header").append("bio");
-//...................
 
 var bio = {
 	"name" : "Jamie",
@@ -39,7 +19,51 @@ var bio = {
 	},
 	"welcomeMessage" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
 	"picture" : "http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg",
-	"Skills" : ["skill", "asdfs", "fddsf"]
+	"skills" : ["html", "illustrator", "tennis"]
+};
+
+var education ={
+	"school":[
+	{
+		"name" : "WSCAD",
+		"location" : "London",
+		"degree": "BA",
+	    "majors": ["photography"],
+	    "dates": 2006,
+	    "url": "http:www.apple.com"
+	}
+	],
+	"onlineCourses" :[
+	{
+		"title" : "FEWD",
+		"school" : "Udacity",
+		"dates": 2016,
+	    "url": "http:www.udacity.com"
+	}
+ 	]
+};
+
+var work ={
+	"jobs":[
+	{
+		"employer" : "Freelance",
+		"title" : "Developer",
+		"location": "HK",
+	    "dates": "In Progress",
+	    "description": "All kinds of stuff"
+	}
+	]
+};
+
+var projects = {
+"proj":[
+	{
+		"title" : "UX design",
+		"dates" : 2016,
+		"description": "UX for website",
+		"images": ["https://img1.wsimg.com/fos/sales/cwh/8/images/cats-with-hats-shop-02.jpg", "https://img1.wsimg.com/fos/sales/cwh/8/images/cats-with-hats-shop-02.jpg"]
+	}
+	]
 };
 
 
@@ -51,48 +75,12 @@ $("#topContacts").append(HTMLlocation.replace("%data%", bio.contact.location));
 $("#header").append(HTMLbioPic.replace("%data%", bio.picture));
 $("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
 
-// var education ={
-// 	"school":[
-// 	{
-// 		"name" : "WSCAD",
-// 		"location" : "London",
-// 		"degree": "BA",
-// 	    "majors": ["photography"],
-// 	    "dates": 2006,
-// 	    "url": "http:www.apple.com"
-// 	}
-// 	],
-// 	"onlineCourses" :[
-// 	{
-// 		"title" : "FEWD",
-// 		"school" : "Udacity",
-// 		"dates": 2016,
-// 	    "url": "http:www.udacity.com"
-// 	}
-//  	]
-// };
-
-// var work ={
-// 	"jobs":[
-// 	{
-// 		"employer" : "Freelance",
-// 		"title" : "Developer",
-// 		"location": "HK",
-// 	    "dates": "In Progress",
-// 	    "description": "All kinds of stuff"
-// 	}
-// 	]
-// };
+if (bio.skills !== 0) {
+	$("#header").append(HTMLskillsStart);
+	$("#header").append(HTMLskills.replace("%data%", bio.skills [0]));
+	$("#header").append(HTMLskills.replace("%data%", bio.skills [1]));
+	$("#header").append(HTMLskills.replace("%data%", bio.skills [2]));
+};
 
 
 
-// var projects = {
-// "proj":[
-// 	{
-// 		"title" : "UX design",
-// 		"dates" : 2016,
-// 		"description": "UX for website",
-// 		"images": ["https://img1.wsimg.com/fos/sales/cwh/8/images/cats-with-hats-shop-02.jpg", "https://img1.wsimg.com/fos/sales/cwh/8/images/cats-with-hats-shop-02.jpg"]
-// 	}
-// 	]
-// };
